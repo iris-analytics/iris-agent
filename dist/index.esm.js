ea7fae3fd5adba1952cb73b74183f6f2207bd90a
Febe Pérez <febe.holowaty@gmail.com>
2020-05-27 14:15:52 +0200

    Add workflow (#20)
    * Refactor current workflow to do a build, that this means that is executing: lint, test and build.
    * Adding to precommit the action to add to git to prevent built files.

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,6 +1,4 @@
 function _typeof(obj) {
-  "@babel/helpers - typeof";
-
   if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
     _typeof = function (obj) {
       return typeof obj;
